bb41daf1c0cbce999739f9151985e72fe7233ea6
Arjun Makkar <arjunmakkar2006@gmail.com>
2025-03-31 19:50:00 +0530

    Solution to Selection Sort for Integers

--- Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -1,21 +1,44 @@
-void selectionSort(int arr[], int n) {
-    int i, j, m;
-    for (i = 0; i < n-1; i++) {
-        m = i;
-        for (j = i+1; j < n; j++) {
-            if (arr[j] < arr[m]) {
-                m = j;
+// void selectionSort(int arr[], int n) {
+//     int i, j, m;
+//     for (i = 0; i < n-1; i++) {
+//         m = i;
+//         for (j = i+1; j < n; j++) {
+//             if (arr[j] < arr[m]) {
+//                 m = j;
+//             }
+//         }
+//         int temp = arr[m];
+//         arr[m] = arr[i];
+//         arr[i] = temp;
+//     }
+// }
+
+// void printArray(int arr[], int n) {
+//     for (int i = 0; i < n; i++) {
+//         printf("%d ", arr[i]);
+//     }
+//     printf("\n");
+// }
+
+
+
+void selectionSort(int arr[],int n){
+    int i,j,m;
+    for(i=0;i<n-1;i++){
+        m=i;
+        for(j=i+1;j<n;j++){
+            if(arr[j]<arr[m]){
+                m=j;
             }
         }
-        int temp = arr[m];
-        arr[m] = arr[i];
-        arr[i] = temp;
+        int temp=arr[m];
+        arr[m]=arr[i];
+        arr[i]=temp;
     }
 }
-
-void printArray(int arr[], int n) {
-    for (int i = 0; i < n; i++) {
-        printf("%d ", arr[i]);
+void printArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        printf("%d ",arr[i]);
     }
     printf("\n");
 }
